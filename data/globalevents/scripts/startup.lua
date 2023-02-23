8551e3bbfd87f110e442e59b16f9a8b8cfa02195
Emilianenko <celohere@gmail.com>
2023-02-23 17:55:53 -0300

    Update startup.lua

--- data/globalevents/scripts/startup.lua
+++ data/globalevents/scripts/startup.lua
@@ -1,44 +1,19 @@
 function onStartup()
 	math.randomseed(os.mtime())
 
-	db.query("TRUNCATE TABLE `players_online`")
-
-
+	db.asyncQuery("DELETE FROM `players_online` WHERE `world_id` = " .. configManager.getNumber(configKeys.WORLD_ID))
 
 	db.asyncQuery("UPDATE `guild_wars` SET `status` = 4, `ended` = " .. os.time() .. " WHERE `status` = 1 AND (`started` + 3* 60 * 60) < " .. os.time())
-
-
 
 	db.asyncQuery("DELETE FROM `players` WHERE `deletion` != 0 AND `deletion` < " .. os.time())
 	db.asyncQuery("DELETE FROM `ip_bans` WHERE `expires_at` != 0 AND `expires_at` <= " .. os.time())
 
-
-
-
-	-- deletar as guilds canceladas e rejeitadas
+	-- Delete canceled and rejected wars
 	db.asyncQuery('DELETE FROM `guild_wars` WHERE `status` = 2')
 	db.asyncQuery('DELETE FROM `guild_wars` WHERE `status` = 3')
 	
-	-- deletar as guilds que estão muito tempo pendentes 3 dias
+	-- Delete pending wars(3 days)
 	db.asyncQuery('DELETE FROM `guild_wars` WHERE `status` = 0 AND (`started` + 72 * 60 * 60) <= ' .. os.time()) 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 	-- Move expired bans to ban history
 	local resultId = db.storeQuery("SELECT * FROM `account_bans` WHERE `expires_at` != 0 AND `expires_at` <= " .. os.time())
@@ -52,7 +27,7 @@ function onStartup()
 	end
 
 	-- Check house auctions
-	local resultId = db.storeQuery("SELECT `id`, `highest_bidder`, `last_bid`, (SELECT `balance` FROM `players` WHERE `players`.`id` = `highest_bidder`) AS `balance` FROM `houses` WHERE `owner` = 0 AND `bid_end` != 0 AND `bid_end` < " .. os.time())
+	local resultId = db.storeQuery("SELECT `id`, `highest_bidder`, `last_bid`, (SELECT `balance` FROM `players` WHERE `players`.`id` = `highest_bidder`) AS `balance` FROM `" .. configManager.getString(configKeys.MYSQL_WORLD_DB) .. "`.`houses` WHERE `owner` = 0 AND `bid_end` != 0 AND `bid_end` < " .. os.time())
 	if resultId ~= false then
 		repeat
 			local house = House(result.getDataInt(resultId, "id"))
@@ -64,9 +39,18 @@ function onStartup()
 					db.query("UPDATE `players` SET `balance` = " .. (balance - lastBid) .. " WHERE `id` = " .. highestBidder)
 					house:setOwnerGuid(highestBidder)
 				end
-				db.asyncQuery("UPDATE `houses` SET `last_bid` = 0, `bid_end` = 0, `highest_bidder` = 0, `bid` = 0 WHERE `id` = " .. house:getId())
+				db.asyncQuery("UPDATE `" .. configManager.getString(configKeys.MYSQL_WORLD_DB) .. "`.`houses` SET `last_bid` = 0, `bid_end` = 0, `highest_bidder` = 0, `bid` = 0 WHERE `id` = " .. house:getId())
 			end
 		until not result.next(resultId)
 		result.free(resultId)
+	end
+	
+	-- Server config
+	local previewerstatus = configManager.getBoolean(configKeys.IS_PREVIEWER) and 1 or 0
+	local resultId = db.storeQuery("SELECT * FROM `servers` WHERE `id` = " .. configManager.getNumber(configKeys.WORLD_ID))
+	if resultId ~= false then
+		db.asyncQuery("UPDATE `servers` SET `name` = '" .. configManager.getString(configKeys.SERVER_NAME) .. "', `ip` = '" .. configManager.getString(configKeys.IP) .. "', `port` = " .. configManager.getNumber(configKeys.GAME_PORT) .. ", `previewer` = " .. previewerstatus .." WHERE `id` = " .. configManager.getNumber(configKeys.WORLD_ID))
+	else
+		db.asyncQuery("INSERT INTO `servers` (`id`, `name`, `ip`, `port`, `previewer`) VALUES (" .. configManager.getNumber(configKeys.WORLD_ID) ..", '" .. configManager.getString(configKeys.SERVER_NAME) .. "', '" .. configManager.getString(configKeys.IP) .. "', " .. configManager.getNumber(configKeys.GAME_PORT) .. ", " .. previewerstatus ..")")
 	end
 end
